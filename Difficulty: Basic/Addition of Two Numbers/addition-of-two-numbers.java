c58a7ada77344b676286985a42b128f2e605d6c9
as7245451-commits <as7245451@gmail.com>
2025-12-26 00:29:25 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Addition of Two Numbers/addition-of-two-numbers.java
@@ -0,0 +1,9 @@
+// User function Template for Java
+// User function Template for Java
+class Solution {public static void main(String[]args){addition(10,20);}
+    static long addition(long a, long b) {
+        // code here
+        long c= a+b;
+        return c;
+    }
+}
